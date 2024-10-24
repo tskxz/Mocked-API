982e74f5f3227773036f9a9c04464df770326be8
Guillaume Gayot <guillaume.gayot@hotmail.fr>
2024-10-24 14:58:16 +0200

    feat(emails): add endpoint for random email using user first name and last name
    Closes #81

--- api/src/modules/emails/api/emails-routes.ts
+++ api/src/modules/emails/api/emails-routes.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
 import * as core from 'express-serve-static-core';
 import { getRandomEmails } from '../utils/getRandomEmails';
+import {getRandomEmailByUsername} from "../utils/getRandomEmailByUsername";
 
 const defaultEmailCount = 10;
 
 module.exports = function (app: core.Express) {
     /**
      * @openapi
-     * '/email':
+     * '/emails':
      *   get:
      *     tags:
      *     - Emails
@@ -20,5 +21,47 @@ module.exports = function (app: core.Express) {
      */
     app.get('/emails', (req: Request, res: Response) => {
         res.json(getRandomEmails(defaultEmailCount));
+    });
+
+    /**
+     * @openapi
+     * '/email':
+     *   get:
+     *     tags:
+     *     - Email
+     *     summary: Returns a movk of email using first and last name
+     *     parameters:
+     *       - in: query
+     *         name: firstName
+     *         required: true
+     *         schema:
+     *           type: string
+     *         description: The first name of the user
+     *       - in: query
+     *         name: lastName
+     *         required: true
+     *         schema:
+     *           type: string
+     *         description: The last name of the user
+     *     responses:
+     *       '200':
+     *         description: OK
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/definitions/MockEmails'
+     *       '400':
+     *         description: Bad Request
+     *         content:
+     *           application/json:
+     */
+
+    app.get('/emails/address', (req: Request, res: Response) => {
+        const firstName = req.query.firstName as string;
+        const lastName = req.query.lastName as string;
+        if (!firstName || !lastName) {
+            return res.status(400).json({ error: 'firstName and lastName are required' });
+        }
+        res.json(getRandomEmailByUsername( firstName.toLowerCase(), lastName.toLowerCase()));
     });
 };
